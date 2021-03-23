a06e96e8d6606ec7fc2b02c6c21121f8180fe9f8
Your Name <you@example.com>
2021-03-23 14:25:32 +0000

    Lists, tuples, and dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -7,6 +7,11 @@ devops_student = {
     "completed_lesson_names": ["variables", "data types", "collections"]
 }
 
+# Add operations to lesson names, increase lessons complete to 4, remove index 1 lesson name
+devops_student["completed_lesson_names"].append("operators")
+devops_student["completed_lesson"] = 4
+devops_student["completed_lesson_names"].remove("data types")
+
 print(devops_student)
 print(type(devops_student))
 print(devops_student["name"])
